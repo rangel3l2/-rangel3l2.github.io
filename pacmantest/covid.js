1326b26a07a95c35a3725861195245aaf2fae8a3
rangel3l2 <rangel.silva@estudante.ifms.edu.br>
2020-06-22 20:23:45 -0400

    modificações de mapa e outras

--- pacmantest/covid.js
+++ pacmantest/covid.js
@@ -2,32 +2,39 @@
 var world = [
         [5,5,5,5,5,5,5,5,5,5,5 ,5 ,5 ,5 ,5 ,5 ,5 ,5 ,5 ,5 ,5 ,5,5],
 	    [5,1,1,1,1,1,1,1,1,1,1, 2, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1,5], 
-		[5,1,2,2,2,2,2,2,2,2,2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1,5], 
-   	    [5,1,2,4,4,4,2,4,4,4,2, 4, 2, 4, 2, 4, 2, 4, 2, 2, 2, 1,5], 
+		[5,1,0,2,2,2,2,2,2,2,2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1,5], 
+   	    [5,1,2,4,4,4,2,4,4,4,2, 4, 2, 4, 2, 4, 2, 4, 2, 1, 2, 1,5], 
 		[5,1,2,4,2,2,2,2,2,2,2, 4, 2, 4, 2, 2, 2, 4, 4, 2, 2, 1,5], 
-		[5,2,2,2,2,2,2,4,2,4,2, 2, 2, 2, 2, 4, 2, 2, 2, 4, 2, 2,5], 
+		[5,2,2,2,2,1,2,4,2,4,2, 2, 2, 2, 2, 4, 2, 2, 2, 4, 2, 2,5], 
     	[5,1,2,4,2,2,2,2,2,2,2, 4, 2, 4, 2, 4, 2, 4, 4, 2, 2, 1,5], 
-		[5,1,2,4,4,4,2,4,4,4,2, 2, 4, 2, 2, 4, 2, 4, 2, 2, 2, 1,5], 
+		[5,1,2,4,4,4,2,4,4,4,2, 2, 4, 2, 2, 4, 2, 4, 2, 1, 2, 1,5], 
 		[5,1,2,2,2,2,2,2,2,2,2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1,5], 
-        [5,1,2,2,2,2,2,2,2,2,2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1,5],  
-        [5,2,2,2,2,2,2,2,2,2,2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,5], 
-		[5,1,2,2,2,2,2,2,2,4,4, 2, 2, 4, 4, 4, 2, 2, 2, 2, 2, 1,5], 
-   	    [5,1,2,2,2,2,2,2,2,4,4, 2, 2, 4, 1, 4, 2, 2, 2, 2, 2, 1,5], 
-		[5,1,2,2,2,2,2,2,2,2,4, 2, 2, 4, 4, 4, 2, 2, 2, 2, 2, 1,5], 
-		[5,2,2,2,2,2,2,2,2,2,4, 2, 2, 2, 2, 4, 2, 2, 2, 2, 2, 2,5], 
-		[5,1,2,2,2,2,2,2,2,2,4, 2, 2, 4, 2, 4, 2, 2, 2, 2, 2, 1,5], 
-		[5,1,2,2,2,2,2,2,2,2,2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1,5], 
-		[5,1,2,2,2,2,2,2,2,2,2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1,5], 
+        [5,1,1,1,1,1,1,1,1,1,2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1,5],  
+        [5,2,2,2,2,2,2,2,2,2,2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2,5], 
+		[5,1,2,1,2,1,1,1,4,4,2, 1, 2, 4, 4, 4, 2, 1, 2, 1, 2, 1,5], 
+   	    [5,1,2,2,2,2,1,2,4,4,2, 1, 2, 4, 1, 4, 2, 2, 2, 1, 2, 1,5], 
+		[5,1,2,1,1,2,2,2,2,4,2, 1, 2, 4, 4, 4, 2, 1, 1, 1, 2, 1,5], 
+		[5,2,2,1,1,1,1,1,2,4,2, 1, 2, 2, 2, 4, 2, 1, 2, 1, 2, 2,5], 
+		[5,1,2,2,2,2,2,2,2,4,2, 1, 2, 4, 2, 4, 2, 1, 2, 1, 2, 1,5], 
+		[5,1,2,1,1,1,1,1,2,1,2, 2, 2, 2, 2, 2, 2, 1, 2, 1, 2, 1,5], 
+		[5,1,2,1,2,2,2,2,2,1,1, 2, 1, 2, 1, 1, 2, 2, 2, 2, 2, 1,5], 
         [5,1,1,1,1,1,1,1,1,1,1, 2, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1,5],        
         [5,5,5,5,5,5,5,5,5,5,5 ,5 ,5 ,5 ,5 ,5 ,5 ,5 ,5 ,5 ,5 ,5,5]
 	];
-
+//22x19
 	var score = 0;
 
 	var covid = {
 		x: 2,
 		y: 2
-	}
+    }
+    const eaten = 0
+    const brick3d = 4
+    const meal =2
+    const brick = 1
+
+
+    
     //como o mapa é estruturado (seus elementos)
 	function drawWorld(){
 		var map = '';
@@ -35,16 +42,16 @@ var world = [
 		for(var i=0; i<world.length; i++){
 			map += "<div class='row'>";
 			for(var j=0; j<world[i].length; j++){
-                if(world[i][j] == 1)
+                if(world[i][j] == brick)
                 
 					map += "<div class='brick'></div>";
-				else if(world[i][j] == 2)
+				else if(world[i][j] == meal)
 					map += "<div class='meal'></div>";
 				else if(world[i][j] == 3)
                     map += "<div class='cherry'></div>";
-                    else if(world[i][j]==4)
+                    else if(world[i][j]==brick3d)
                     map+= "<div class='covidLogo'></div>";
-				else if(world[i][j] == 0)
+				else if(world[i][j] == eaten)
                     map += "<div class='empty'></div>";
                     else if(world[i][j] == 5)
                     map += "<div class='limit'></div>";    
@@ -79,24 +86,35 @@ var world = [
         
         document.getElementById('score').innerHTML = (`Score<br>${score}</br>`);
         document.querySelector('#score').style.display='block';
+       
     }
     //caminho inicial do código
     window.addEventListener("load",main);
+    
     //1. inicio do código 
 
     function main(){    
      
      document.querySelector('#iniciar').innerHTML =
       (`<input type="button"  value="play game">`)
-         addEventListener("click",Start)        
+         addEventListener("click",startClick)        
          addEventListener("click",interval)
        
     }
     
 //eventos iniciais
-    function Start(){
+    function startClick(){
         addEventListener("keydown",mykeyBoard,false);
-        document.querySelector('#iniciar').style.display="none";   
+        document.querySelector('#iniciar').style.display="none";  
+        document.querySelector('#poison').style.display="block"; 
+        document.addEventListener("load",movePoison);
+      
+
+        const audio1 = document.getElementById('mostrar')
+         audio1.play();
+         audio1.currentTime =40;
+         audio1.volume = 0.1;
+         audio1.duration=2;
             
      drawWorld()
     }
@@ -105,23 +123,180 @@ var world = [
     const btn_right=39;
     const btn_up=38;
     const btn_down=40;
-    var dxr=0,dxl=0,dyu=0,dyd=0;
+    var dxr=false,dxl=false,dyu=false,dyd=false;
     const Timer =500;
     let interLeft,interRight,interUp,interDown;
     let pressTime=0
+    var test= []
+   var e
+   let directionY=false;
+   let directionX=false;
+   let left=true,right=true,up=true,down=true;
+  
+   function direction(){
+       
+   } 
 	function mykeyBoard(e){
+        let front = world[covid.y][covid.x+1];
+        let back =  world[covid.y][covid.x-1];
+        let over = world[covid.y-1][covid.x];
+        let under = world[covid.y+1][covid.x];
+       //movimento normal 
+      if(e.keyCode==btn_right && front=== brick || front === brick3d){
+          right=false;
+          console.log("right",right);
+
+      }
+      else if(e.keyCode==btn_right && front=== eaten || front === meal){
+          right=true;
+         console.log("right",right);
+         
+       
+      }
+      else if(e.keyCode==left && back=== brick || back ===brick3d){
+          left=false
+          console.log("left",left);
+          
+          
+      }
+      else if(e.keyCode==btn_left && back === eaten || back ===meal){
+          left=true
+          console.log("left",left);
+          
+      }
+      else if(e.keyCode==btn_up && over === brick || over ===brick3d){
+          up=false;
+        console.log("up",up);
+              }
+      else if(e.keyCode==btn_up && over === meal || over === eaten){
+          up =true
+        console.log("up",up);
+        
+      }
+      else if(e.keyCode==btn_down && under === brick || under === brick3d){
+          down=false
+         console.log("down",down);
+         
+      }
+      else if(e.keyCode==btn_down && under === meal || under === eaten){
+          down=true;
+          console.log("down",down);
+          
+      }
+
+      //edge left
+      
+      else if(e.keyCode==btn_left && over==brick || over==brick3d){
+          up=false;
+          console.log("up permitido",up);
+      }
+      else if(e.keyCode==btn_left && under==brick|| under==brick3d){
+          down=false;
+          console.log("down permitido",down);
+      }
+      else if(e.keyCode==btn_left && over == meal || over==eaten){
+          up=true
+          console.log("up permitido",up);
+      }
+      else if(e.keyCode==btn_left && under == meal || under == eaten ){
+          down=true;
+          console.log("down permitido",down);
+          
+      }
+    //edge right
+    else if(e.keyCode==btn_right && over==brick || over==brick3d){
+        up=false;
+        console.log("up negado",up);
+    }
+    else if(e.keyCode==btn_right && under==brick|| under==brick3d){
+        down=false;
+        console.log("down negado",down);
+    }
+    else if(e.keyCode==btn_right && over == meal || over==eaten){
+        up=true
+        console.log("up permitido",up);
+    }
+    else if(e.keyCode==btn_right && under == meal || under == eaten ){
+        down=true;
+        console.log("down permitido",down);
+        
+    }
+    //edge up
+    else if(e.keyCode==btn_up && front==brick || front==brick3d){
+       right=false
+        console.log("right negado",right);
+
+        
+    }
+    else if(e.keyCode==btn_up && back==brick || back==brick3d){
+        left=false;
+        console.log("left negado",left);
+        
+    }
+    else if(e.keyCode==btn_up && front ==meal || front==eaten){
+        right=true;
+        console.log("right permitido",right);
+    }
+    else if(e.keyCode==btn_up && back == meal || back == eaten){
+        left=true;
+        console.log("left permitido",left);
+        
+    }
+    // edge down
+    else if(e.keyCode==btn_down && front==brick || front==brick3d){
+        right=false
+         console.log("right negado",right);
+ 
+         
+     }
+     else if(e.keyCode==btn_down && back==brick || back==brick3d){
+         left=false;
+         console.log("left negado",left);
+         
+     }
+     else if(e.keyCode==btn_down && front ==meal || front==eaten){
+         right=true;
+         console.log("right permitido",right);
+     }
+     else if(e.keyCode==btn_down && back == meal || back == eaten){
+         left=true;
+         
+         console.log("left permitido",left);
+         
+     }
+
+        
+      
+ 
+else if( e.keyCode ==btn_left || e.keyCode ==btn_right){
+directionX = true;
+directionY=false;
+}
+else if(e.keyCode == btn_up || e.keyCode == btn_down){
+directionY=true;
+directionX=false;
+}
+    
+      
+           
+           
+             
+             
+       
+        
+       
 		//document.getElementById('covid').style.left = 50+"px";
         //console.log(covid.x);
         
-        if(e.keyCode == btn_left){
+        if(e.keyCode ==btn_left ){
             
             pressTime++;
           
             setTimeout(() => (pressTime = 0), Timer)
-            dxl=1
-            dxr=0
-            dyu=0
-            dyd=0
+            dxl=true
+            dxr=false
+            dyu=false
+            dyd=false
            
             moveLeft()
            
@@ -130,13 +305,14 @@ var world = [
 }
         
         else if(e.keyCode == btn_right ){
+          
             pressTime++;
           
             setTimeout(() => (pressTime = 0), Timer)
-            dxl=0
-            dxr=1
-            dyu=0
-            dyd=0
+            dxl=false
+            dxr=true
+            dyu=false
+            dyd=false
            
             moveRight();
            
@@ -151,40 +327,50 @@ var world = [
        
         else if(e.keyCode == btn_up ){
 
+           
             pressTime++;
           
             setTimeout(() => (pressTime = 0), Timer)
-            dxr=0
-            dxl=0
-            dyu=1
-            dyd=0
+            dxr=false
+            dxl=false
+            dyu=true
+            dyd=false
             
             moveUp()
        
         }
        
-        else if(e.keyCode == btn_down){
+        else if(e.keyCode == btn_down ){
             pressTime++;
-          
+         
             setTimeout(() => (pressTime = 0), Timer)
-		    dxr=0
-            dxl=0
-            dyu=0
-            dyd=1
+		    dxr=false
+            dxl=false
+            dyu=false
+            dyd=true
         
           moveDown()
           
+     
+       
+       
+       
        
         }
         
       //comer elementos
 	
         //console.log(e.keyCode);
-        
+        console.log("left externo",left);
+        console.log("right externo",right);
+        console.log("up externo", up);
+        console.log("down externo",down,"\n\n\n")
+       
 		displayCovid();
     }
     //realizadas tarefas relacionados ao movimento do objeto pacman
     function moveLeft(){
+     
         allowMoveLeft();                     
         crossWorldLeft();
         displayCovid();
@@ -196,6 +382,7 @@ var world = [
                
     }
     function moveRight(){
+
         allowMoveRight();
         crossWorldRight();
         displayCovid();        
@@ -220,10 +407,10 @@ var world = [
     }
    //condições para acontecer o movimento
     function allowMoveLeft(){
-        
+        test.push(dxl)
         if(world[covid.y][covid.x-1] != 1 && 
              world[covid.y][covid.x-1]!= 4 
-             && dxl==1 && pressTime==0               
+             && dxl && pressTime==0  && left             
             )
             {
                
@@ -231,7 +418,7 @@ var world = [
                 covid.x-=dxl;
                 
                   
-                  
+                
             
                    
                     document.getElementById('covid').style.left =  document.getElementById('covid').style.transform = "scaleX(-1)";			
@@ -240,11 +427,11 @@ var world = [
               
                 
             }
-            
-    }
+        }
     function allowMoveRight(){
+        test.push(dxr)
         if( world[covid.y][covid.x+1] != 1 && covid.x<=world[0].length &&
-            world[covid.y][covid.x+1] != 4 && dxr==1 && pressTime==0   )
+            world[covid.y][covid.x+1] != 4 && dxr && pressTime==0 && right   )
             {
                 document.getElementById('covid').style.transform = "none";
               
@@ -252,20 +439,31 @@ var world = [
                covid.x=covid.x+dxr;
                
             }
+       
     }
     function allowMoveUp(){
+        test.push(dyu)
         if(world[covid.y-1][covid.x]!= 1 && covid.y > 0 &&
-            world[covid.y-1][covid.x] != 4 &&dyu==1 && pressTime==0   ) 
+            world[covid.y-1][covid.x] != 4 &&dyu && pressTime==0 &&up  ) 
             {
                 document.getElementById('covid').style.transform = "rotate(-90deg)";
                
                 covid.y=covid.y-dyu;
                 
+                
+                
             } 
+
     }
     function allowMoveDown(){
+       
         if(world[covid.y+1][covid.x]  != 1 &&
-            world[covid.y+1][covid.x] != 4 && dyd==1 && pressTime==0   )
+            world[covid.y+1][covid.x] != 4 && dyd && pressTime==0 
+            && covid.y<world.length && down
+           )
+          
+            
+              
             {
                 document.getElementById('covid').style.transform = "rotate(90deg)";
                
@@ -288,7 +486,8 @@ var world = [
          if(world[covid.y][covid.x+1]!= 1 &&  covid.x > 0 && covid.x == world[0].length -1
              && world[covid.y][covid.x+1] != 4 && world[covid.y][covid.x+1] != 0 ){
 			document.getElementById('covid').style.transform = "none";
-			covid.x = 1;
+            covid.x = 1;
+            
         }
     }
     function crossWorldUp(){   
@@ -318,6 +517,7 @@ var world = [
             displayScore();
             const audio = document.getElementById('comer')
             audio.play();
+
             
            
         }
@@ -328,6 +528,7 @@ var world = [
     if (world[covid.y][covid.x] == 0){
         
         score+=10;
+      
     }
     
    
@@ -342,5 +543,77 @@ function interval(){
      interRight = setInterval(moveRight,Timer);
      interUp = setInterval(moveUp,Timer);
      interDown=setInterval(moveDown,Timer);
+
+     interLeft = setInterval(allowMovePoisonLeft, Timer);
+     interRight = setInterval(allowMovePoisonRight,Timer);
+     interUp = setInterval(allowMovePoisonUp,Timer);
+     interDown=setInterval(allowMovePoisonDown,Timer);
+
+     
 }
 
+
+
+let poison ={
+    y:17,
+    x:17
+}
+function displayPoison(){
+      
+        
+
+    document.getElementById('poison').style.left = poison.x*25.2+"px";
+ 
+    
+    document.getElementById('poison').style.top = poison.y*25.5+"px";
+
+
+
+}         
+let iniciador
+function movePoison(){
+ iniciador= Math.floor(Math.random() * 4);
+allowMovePoisonLeft()
+allowMovePoisonRight()
+allowMovePoisonUp()
+allowMovePoisonDown()
+displayPoison()
+console.log(iniciador);
+
+
+}
+ 
+function allowMovePoisonLeft(){
+    if(world[poison.y][poison.x-1]!=1 &&
+        world[poison.y][poison.x-1]!=4 && poison.x > 0 &&
+        iniciador==0){
+            poison.x--;
+        }
+
+}
+function allowMovePoisonRight(){
+    if(world[poison.y][poison.x+1]!=1 &&
+        world[poison.y][poison.x+1]!=4 &&
+        poison.x<=world[0].length && iniciador==1 ){
+            poison.x++;
+        }
+
+}
+function allowMovePoisonUp(){
+    if(world[poison.y-1][poison.x]!=1 
+        &&world[poison.y-1][poison.x]!=4
+         && poison.y > 0 && iniciador==2){
+            poison.y--;
+        }
+
+}
+function allowMovePoisonDown(){
+    if(world[poison.y+1][poison.x]!=1 
+        &&world[poison.y+1][poison.x]!=4 &&
+        iniciador==3
+       ){
+
+            poison.y++;
+        }
+
+}
---
+++ pacmantest/teste.Functions.js
@@ -0,0 +1,23 @@
+let score;
+
+let emo =(score) => score+10
+
+    
+
+
+var n =20
+
+casa(n);
+
+function casa(n){
+    if(n<40){
+    
+   return casa(n-1);
+    }
+
+}
+function mansão(){
+    
+n=n+10
+console.log(casa())
+}
